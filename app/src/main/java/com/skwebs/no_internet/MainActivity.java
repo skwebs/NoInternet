997b45f33295b279f6ecb3191c848d85d16c0963
Satish Kumar Sharma <skwebserv@gmail.com>
2021-12-06 23:29:53 +0530

    fixed an issue

--- app/src/main/java/com/skwebs/no_internet/MainActivity.java
+++ app/src/main/java/com/skwebs/no_internet/MainActivity.java
@@ -43,9 +43,10 @@ public class MainActivity extends AppCompatActivity {
     };
 
     @Override
-    protected void onPause() {
-        super.onPause();
+    protected void onDestroy() {
+        super.onDestroy();
         unregisterReceiver(broadcastReceiver);
     }
+
 }
 
