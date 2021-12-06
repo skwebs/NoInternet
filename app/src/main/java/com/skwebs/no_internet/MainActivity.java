1d9a043c7e9276d2c9a57462e95fcd3b0074c8e6
Satish Kumar Sharma <skwebserv@gmail.com>
2021-12-06 23:15:36 +0530

    Successfully implement NoInternet action with BroadcasterReceiver

--- app/src/main/java/com/skwebs/no_internet/MainActivity.java
+++ app/src/main/java/com/skwebs/no_internet/MainActivity.java
@@ -1,30 +1,46 @@
 package com.skwebs.no_internet;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    BroadcastReceiver broadcastReceiver = null;
+    RelativeLayout internetLayout, noInternetLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        broadcastReceiver = new ConnectivityReceiver();
-        internetStatus();
+        registerReceiver(broadcastReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+        internetLayout = findViewById(R.id.internet_layout);
+        noInternetLayout = findViewById(R.id.no_internet_layout);
     }
 
-    public void internetStatus(){
-        registerReceiver(broadcastReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
-    }
+    final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            boolean status = CheckInternet.getNetworkInfo(context);
+            if(status) {
+                internetLayout.setVisibility(View.VISIBLE);
+                noInternetLayout.setVisibility(View.GONE);
+                Toast.makeText(context, "connected", Toast.LENGTH_SHORT).show();
+            }else {
+                Toast.makeText(context, "not connected", Toast.LENGTH_SHORT).show();
+                internetLayout.setVisibility(View.GONE);
+                noInternetLayout.setVisibility(View.VISIBLE);
+            }
+        }
+
+    };
 
     @Override
     protected void onPause() {
@@ -33,14 +49,3 @@ public class MainActivity extends AppCompatActivity {
     }
 }
 
-class ConnectivityReceiver extends BroadcastReceiver {
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        boolean status = CheckInternet.getNetworkInfo(context);
-        if(status){
-            Toast.makeText(context, "Connected", Toast.LENGTH_LONG).show();
-        }else{
-            Toast.makeText(context, "Not Connected.", Toast.LENGTH_SHORT).show();
-        }
-    }
-}
